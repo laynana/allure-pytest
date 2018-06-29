b00c2ad8c4ae5cfd173627e2780bce1cb80fc7bc
LAPTOP-2H2OKT8Q\lenovo <lana463684@163.com>
2018-06-29 20:40:22 +0800

    2次更改

---
+++ scripet/test_all.py
@@ -0,0 +1,13 @@
+class Test_allure:
+    def test_ai(self):
+        pass
+    def test_aj(self):
+        pass
+    def test_al(self):
+        assert 0
+    def test_ab(self):
+        assert 1
+    def test_av(self):
+        assert 1
+    def test_an(self):
+        assert 1
